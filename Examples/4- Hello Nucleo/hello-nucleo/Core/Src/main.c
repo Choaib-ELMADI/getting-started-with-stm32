7e221b6d82826c5293cdd6f9b02dffc8c752bdd3
Choaib ELMADI <choaibamd@gmail.com>
2025-02-06 20:20:04 +0100

    Edit auto generated code

--- Examples/4- Hello Nucleo/hello-nucleo/Core/Src/main.c
+++ Examples/4- Hello Nucleo/hello-nucleo/Core/Src/main.c
@@ -63,8 +63,7 @@ static void MX_USART2_UART_Init(void);
   * @brief  The application entry point.
   * @retval int
   */
-int main(void)
-{
+int main(void) {
 
   /* USER CODE BEGIN 1 */
 
@@ -95,11 +94,12 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  while (1)
-  {
+  while (1) {
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+	HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+	HAL_Delay(500);
   }
   /* USER CODE END 3 */
 }
