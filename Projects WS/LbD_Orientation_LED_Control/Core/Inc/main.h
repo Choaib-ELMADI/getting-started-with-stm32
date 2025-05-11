600122c12b0d380234f6ec25f831d06dcdd0cecc
Choaib ELMADI <choaibamd@gmail.com>
2025-05-11 15:56:36 +0100

    I2C peripheral master configuration

--- Projects WS/LbD_Orientation_LED_Control/Core/Inc/main.h
+++ Projects WS/LbD_Orientation_LED_Control/Core/Inc/main.h
@@ -57,6 +57,10 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define MPU6050_SCL_Pin GPIO_PIN_6
+#define MPU6050_SCL_GPIO_Port GPIOB
+#define MPU6050_SDA_Pin GPIO_PIN_7
+#define MPU6050_SDA_GPIO_Port GPIOB
 
 /* USER CODE BEGIN Private defines */
 
