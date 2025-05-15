7b628fbd5c742bb3ed1f86ac112d5399dd8fbfc7
Choaib ELMADI <choaibamd@gmail.com>
2025-05-15 22:44:12 +0100

    Read MPU6050 accelerometer data

--- Projects WS/LbD_Orientation_LED_Control/Core/Inc/mpu6050.h
+++ Projects WS/LbD_Orientation_LED_Control/Core/Inc/mpu6050.h
@@ -3,17 +3,29 @@
 
 #include "main.h"
 
-#define MPU6050_WHOAMI_REG          ((uint8_t)117)
-#define MPU6050_PWR_MNG_REG         ((uint8_t)107)
-#define I2C_TIMEOUT                 10UL
+#define MPU6050_WHOAMI_REG          	((uint8_t)117)
+#define MPU6050_PWR_MNG_REG         	((uint8_t)107)
+#define I2C_TIMEOUT                 	10UL
+#define ACCELEROMETER_REG_BASE_ADDRESS	((uint8_t)59)
 
 typedef enum {
 	MPU6050_OK    = 0x00U,
 	MPU6050_ERROR = 0x01U
 } mpu6050_status_t;
 
+typedef struct {
+	int16_t x;
+	int16_t y;
+	int16_t z;
+} mpu6050_accelerometer_data_t;
+
 mpu6050_status_t mpu6050_read_byte(I2C_HandleTypeDef*, uint8_t, uint8_t*);
+mpu6050_status_t mpu6050_read(I2C_HandleTypeDef*, uint8_t, uint8_t*, uint32_t);
+
 mpu6050_status_t mpu6050_write_byte(I2C_HandleTypeDef*, uint8_t, uint8_t);
+
 mpu6050_status_t mpu6050_init(I2C_HandleTypeDef*, uint8_t);
+
+mpu6050_status_t mpu6050_read_accelerometer_data(I2C_HandleTypeDef*, uint8_t, mpu6050_accelerometer_data_t*);
 
 #endif // INC_MPU6050_H_
--- Projects WS/LbD_Orientation_LED_Control/Core/Src/mpu6050.c
+++ Projects WS/LbD_Orientation_LED_Control/Core/Src/mpu6050.c
@@ -2,6 +2,21 @@
 
 static uint8_t mpu6050_i2c_address;
 
+mpu6050_status_t mpu6050_read_byte(I2C_HandleTypeDef *hi2c, uint8_t register_address, uint8_t *data) {
+    HAL_StatusTypeDef status = HAL_I2C_Mem_Read(hi2c, mpu6050_i2c_address << 1, register_address, 1, data, 1, I2C_TIMEOUT);
+    return (status == HAL_OK) ? MPU6050_OK : MPU6050_ERROR;
+}
+
+mpu6050_status_t mpu6050_read(I2C_HandleTypeDef *hi2c, uint8_t register_base_address, uint8_t *buffer, uint32_t nbytes) {
+    HAL_StatusTypeDef status = HAL_I2C_Mem_Read(hi2c, mpu6050_i2c_address << 1, register_base_address, 1, buffer, nbytes, I2C_TIMEOUT);
+    return (status == HAL_OK) ? MPU6050_OK : MPU6050_ERROR;
+}
+
+mpu6050_status_t mpu6050_write_byte(I2C_HandleTypeDef *hi2c, uint8_t register_address, uint8_t data) {
+    HAL_StatusTypeDef status = HAL_I2C_Mem_Write(hi2c, mpu6050_i2c_address << 1, register_address, 1, &data, 1, I2C_TIMEOUT);
+    return (status == HAL_OK) ? MPU6050_OK : MPU6050_ERROR;
+}
+
 mpu6050_status_t mpu6050_init(I2C_HandleTypeDef *hi2c, uint8_t i2c_slave_address) {
     mpu6050_i2c_address = i2c_slave_address;
 
@@ -29,12 +44,18 @@ mpu6050_status_t mpu6050_init(I2C_HandleTypeDef *hi2c, uint8_t i2c_slave_address
 	return MPU6050_OK;
 }
 
-mpu6050_status_t mpu6050_read_byte(I2C_HandleTypeDef *hi2c, uint8_t register_address, uint8_t *data) {
-    HAL_StatusTypeDef status = HAL_I2C_Mem_Read(hi2c, mpu6050_i2c_address << 1, register_address, 1, data, 1, I2C_TIMEOUT);
-    return (status == HAL_OK) ? MPU6050_OK : MPU6050_ERROR;
-}
+mpu6050_status_t mpu6050_read_accelerometer_data(I2C_HandleTypeDef *hi2c, uint8_t i2c_slave_address, mpu6050_accelerometer_data_t *accelerometer_data) {
+	uint8_t raw_data[6];
 
-mpu6050_status_t mpu6050_write_byte(I2C_HandleTypeDef *hi2c, uint8_t register_address, uint8_t data) {
-    HAL_StatusTypeDef status = HAL_I2C_Mem_Write(hi2c, mpu6050_i2c_address << 1, register_address, 1, &data, 1, I2C_TIMEOUT);
-    return (status == HAL_OK) ? MPU6050_OK : MPU6050_ERROR;
+	mpu6050_status_t status = mpu6050_read(hi2c, ACCELEROMETER_REG_BASE_ADDRESS, (uint8_t*)raw_data, sizeof(raw_data));
+	if (status != MPU6050_OK) {
+		printf("Error reading accelerometer data!\n");
+		return MPU6050_ERROR;
+	}
+
+	accelerometer_data->x = (int16_t)(raw_data[0] << 8 | raw_data[1]);
+	accelerometer_data->y = (int16_t)(raw_data[2] << 8 | raw_data[3]);
+	accelerometer_data->z = (int16_t)(raw_data[4] << 8 | raw_data[5]);
+
+	return MPU6050_OK;
 }
